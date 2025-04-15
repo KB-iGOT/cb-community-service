61db20e561c24b6c9ac8cac1357f53d32872f0f3
ruksanasemeir <ruksana.semeir@tarento.com>
2025-04-15 17:22:10 +0530

    Added error msg for update and publish methods aswell

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -242,7 +242,7 @@ public class Constants {
     public static final String MODERATOR_NAME = "moderator_name";
     public static String CommunityCreationAllowed = "isCommunityCreationAllowed";
     public static String CREATE_ERROR_MSG_WITHIN_COMMUNITY = "A community with this name already exists. Please choose a different name to continue.";
-    public static String CREATE_ERROR_MSG_COMMUNITY="A community with this name already exists. Please choose a different name to continue.";
+    public static String CREATE_ERROR_MSG_COMMUNITY="Community name already exists. Do you still want to continue?";
 
     private Constants() {
     }

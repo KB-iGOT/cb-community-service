4489da9945305281dda3eb30568b90fdf4483063
ajay-vishwkarma <ajay.vishwkarma@tarento.com>
2025-05-30 15:25:08 +0530

    KB-9862 | resolved review comment

--- src/main/java/com/igot/cb/authentication/util/KeyManager.java
+++ src/main/java/com/igot/cb/authentication/util/KeyManager.java
@@ -71,10 +71,9 @@ public class KeyManager {
     public static PublicKey loadPublicKey(String key) throws Exception {
         String publicKey = new String(key.getBytes(), StandardCharsets.UTF_8);
         // Remove header and footer from the key string
-        publicKey = publicKey.replace("-----BEGIN PUBLIC KEY-----", "")
-                .replace("-----END PUBLIC KEY-----", "")
-                .replaceAll("[\\r\\n]+", "");
-
+        publicKey = publicKey.replaceAll("(-+BEGIN PUBLIC KEY-+)", "");
+        publicKey = publicKey.replaceAll("(-+END PUBLIC KEY-+)", "");
+        publicKey = publicKey.replaceAll("[\\r\\n]+", "");
         // Decode the key string from Base64
         byte[] keyBytes = Base64Util.decode(publicKey.getBytes("UTF-8"), Base64Util.DEFAULT);
         // Convert the key bytes to a PublicKey object

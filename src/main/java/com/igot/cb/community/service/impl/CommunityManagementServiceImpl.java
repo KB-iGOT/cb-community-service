394860eab75d642cf92dfe6418d5f765264787e5
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-09-29 19:53:39 +0530

    KB-11183::MDO search API enhancement

--- src/main/java/com/igot/cb/community/service/impl/CommunityManagementServiceImpl.java
+++ src/main/java/com/igot/cb/community/service/impl/CommunityManagementServiceImpl.java
@@ -1958,9 +1958,14 @@ public class CommunityManagementServiceImpl implements CommunityManagementServic
                 return response;
             }
             return searchCommunityFromEs(searchCriteria, response, communityIndex);
+        } catch (RuntimeException e) {
+            log.error("Validation error in search:", e);
+            createErrorResponse(response, e.getMessage(),
+                    HttpStatus.BAD_REQUEST, Constants.FAILED_CONST);
+            return response;
         } catch (Exception e) {
             logger.error("Error occured while searching:", e);
-            throw new CustomException(Constants.ERROR, "error while processing",
+            throw new CustomException(Constants.ERROR, "error while processing" +e.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -2079,8 +2084,8 @@ public class CommunityManagementServiceImpl implements CommunityManagementServic
             return response;
         } catch (Exception e) {
             logger.error("Exception occured while fetching and caching in search API:", e);
-            throw new CustomException(Constants.ERROR, "error while processing",
-                HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new CustomException(Constants.ERROR, "error while processing "+ e.getMessage(),
+                HttpStatus.BAD_REQUEST);
         }
     }
 
